f471b0beeb8d68273903b48d10e5cc9286024228
Kilian Maigrot <kilian.maigrot@gmail.com>
2024-02-02 16:16:27 +0100

    Ajout de useReducer (sur formRegister uniquement pour le moment)

--- notepad.js
+++ notepad.js
@@ -1,21 +1,14 @@
-// // ESLint
+// // ESLint OK
 
 // Rajouter titres de pages dans les trads OK
-// App.ts Ok
+// App.ts OK
+
 // GitLab perso OK
+// complexity OK set to 5
+
+// /!\ Usereducer /!\ OK sur formLogin, a appliquer sur les autres
 
 
-// complexity
-// /!\ Usereducer /!\
 // Export des constantes des compos dans un autre doc
 // Tests Jest / React testing Library
 // https://testing-library.com/docs/react-testing-library/intro/
-
-// Merge form changement
-
-
-// "paths": {
-//     "*": ["types/*"]
-//   },
-
-// $2a$10$LkPGLrWvTur7QW0azjQ/LOuD7yMS.6QdqHC7pHx2ko/.nyD3VYYMG
